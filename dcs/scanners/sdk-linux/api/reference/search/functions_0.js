f8f3ac842e60487cf458d03707b46274a9d1cd6d
Eranga Tennakoon <gqcd86@zebra.com>
2022-02-09 08:29:36 +0530

    [DCS Scanner Tools] Scanner SDK for Linux Updates
    API document and PI 4.21 release details updated.

--- dcs/scanners/sdk-linux/api/reference/search/functions_0.js
+++ dcs/scanners/sdk-linux/api/reference/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['close',['Close',['../d6/d06/Cslibcorescanner__xml_8h.html#a7c65088ed7c51a0b3d57e53b5705a520',1,'Cslibcorescanner_xml.h']]]
+  ['execcommand',['ExecCommand',['../d6/d06/Cslibcorescanner__xml_8h.html#addcb57e77683fff80b1f4331e049f2c2',1,'Cslibcorescanner_xml.h']]]
 ];
