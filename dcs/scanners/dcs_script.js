c527009709ce1541361a7e1f82e6f2e245942cef
Eranga Tennakoon <gqcd86@zebra.com>
2025-01-22 14:28:46 +0530

    [SSDK-20773] PowerShell Script Documentation for Windows.
    Document all the PowerShell scripts created for Windows SDK and CoreScanner.

--- dcs/scanners/dcs_script.js
+++ dcs/scanners/dcs_script.js
@@ -3,9 +3,14 @@
     Description     : DCS content specific JS.
     Author          : Eranga Tennakoon - GQCD86 
     Release Notes   :
+        01/22/2025 - Code snippets copy and PowerShell file download support added.
         05/12/2024 - Initial document. Tab control support added
 */
 
+/*
+    Manage tabs on DCS Scanner home page (dcs/scanners/index.html).
+    Make sure to display only on tab content at time.
+*/
 function openTab(contentId) {
     var tabContainer = event.currentTarget.closest('.tab-container');
     var tabs = tabContainer.querySelectorAll('.tab');
@@ -18,6 +23,10 @@ function openTab(contentId) {
     document.getElementById(contentId).classList.add('active');
 }
 
+/*
+    Copy the code snippet to clipboard. Button animation implemented between 'Copy' and 'Copied'.
+    Time intervals can be adjusted as needed. Refer to the comments below.
+ */
 function copyCode(button) {
     const code = document.getElementById('code-snippet').innerText;
     navigator.clipboard.writeText(code).then(() => {
@@ -30,16 +39,21 @@ function copyCode(button) {
     });
 }
 
-
+/*
+    Create the download file using the code block specified in <code></code> tags.
+    Using this I've avoid keeping the physical files in the server.
+*/
 function downloadFile(filename) {
     const code = document.getElementById('code-snippet').innerText;
     const blob = new Blob([code], { type: 'text/plain' });
     const url = URL.createObjectURL(blob);
+    
     const a = document.createElement('a');
     a.href = url;
-    a.download = filename; // Use the passed filename
+    a.download = filename; // Use the passed filename, as the name of the download file.
     document.body.appendChild(a);
     a.click();
+
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
 }
